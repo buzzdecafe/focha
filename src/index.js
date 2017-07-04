d09bf436671f9bcb86471975f2cb5d557cb67288
buzzdecafe <m_hur@yahoo.com>
2017-07-04 11:56:49 -0400

    not tested or proven, but how i would proceed to making async

--- src/index.js
+++ src/index.js
@@ -34,82 +34,81 @@ function focha (options) {
   log('starting focha with options')
   log(JSON.stringify(options, null, 2))
 
-  const prevFailingTests = options.all ? Promise.resolve(undefined)
-    : cache.load().then(
-      resp => {
+  const specFilenames = getSpecs(options);
 
-      },
-      err => {
+  const prevFailingTests = options.all ? Promise.resolve([])
+    : cache.load().then(
+      resp => resp.body.failures,
+      err  => {
         if (!options.all) {
           console.log('😃 no previously failing tests found')
           console.log('ℹ️ run all tests using --all flag')
-          throw err;
         }
+        throw err;
       }
     )
+    .then(
+        prevFails => {
+          if (prevFails.length > 0) {
+            mocha.suite.beforeAll(function () {
+              la(prevFails, 'missing failing tests')
+              log('leaving only failed tests from last run')
+              order.leave(prevFails)(mocha.suite)
 
-  const failedTests = []
-
-
-  const wasRunningJustFailingTests = () =>
-    Maybe.toMaybe(is.unemptyArray(prevFailingTests)
-      ? prevFailingTests : undefined)
-  const numberOfPreviouslyFailingTests = () =>
-    wasRunningJustFailingTests().map(prop('length'))
-
-  if (prevFailingTests) {
-    mocha.suite.beforeAll(function () {
-      la(prevFailingTests, 'missing failing tests')
-      log('leaving only failed tests from last run')
-      order.leave(prevFailingTests)(mocha.suite)
-
-      // the order might be out of date if any tests
-      // were added or deleted, thus
-      // always collect the order
-      // const testNames = order.collect(mocha.suite)
-      // cache.save(testNames)
-    })
-  }
-
-  const runner = mocha.run(function (failures) {
-    // console.log('mocha run finished')
-    // why do we register process.on('exit') here and not
-    // control the exit ourselves?
-    process.on('exit', function () {
-      if (failures === 0) {
-        log('there were no failures in this run')
-        cache.clear()
-        numberOfPreviouslyFailingTests()
-          .forEach(n => {
-            console.log('🤔 previously %s failed', pluralize('test', n, true))
-            console.log('✅ now everything is fine')
-          })
-      } else {
-        if (is.not.empty(failedTests)) {
-          console.log('%d failed tests', failedTests.length)
-          console.log(failedTests)
-          const filename = cache.filename()
-          la(is.unemptyString(filename), 'missing save filename')
-          cache.save({tests: failedTests, version: pkg.version})
-          console.error('Failed tests order saved in', chalk.yellow(filename))
-          console.error('If you run Focha again, failed tests will run first')
-        } else {
-          console.error('Problem: Mocha has finished with an error')
-          console.error('but we have no failed tests recorded! 🐞')
+              // the order might be out of date if any tests
+              // were added or deleted, thus
+              // always collect the order
+              // const testNames = order.collect(mocha.suite)
+              // cache.save(testNames)          
+            });
+          }
         }
-      }
-      process.exit(failures)
+        return prevFails;
     })
-  })
+    .then(fails => {
+      var failedTests = [];
+      const runner = mocha.run(function (failures) {
+        // console.log('mocha run finished')
+        // why do we register process.on('exit') here and not
+        // control the exit ourselves?
+        process.on('exit', function () {
+          if (failures === 0) {
+            log('there were no failures in this run')
+            cache.clear()
+            numberOfPreviouslyFailingTests()
+              .forEach(n => {
+                console.log('🤔 previously %s failed', pluralize('test', n, true))
+                console.log('✅ now everything is fine')
+              })
+          } else {
+            if (is.not.empty(failedTests)) {
+              console.log('%d failed tests', failedTests.length)
+              console.log(failedTests)
+              const filename = cache.filename()
+              la(is.unemptyString(filename), 'missing save filename')
+              cache.save({tests: failedTests, version: pkg.version})
+              console.error('Failed tests order saved in', chalk.yellow(filename))
+              console.error('If you run Focha again, failed tests will run first')
+            } else {
+              console.error('Problem: Mocha has finished with an error')
+              console.error('but we have no failed tests recorded! 🐞')
+            }
+          }
+          process.exit(failures)
+        });
 
-  runner.on('fail', function (test, err) {
-    const failed = {
-      title: test.title,
-      fullTitle: test.fullTitle()
-    }
-    log('failed test "%s"', failed.fullTitle)
-    failedTests.push(failed)
-  })
+    });
+    runner.on('fail', function (test, err) {
+      const failed = {
+        title: test.title,
+        fullTitle: test.fullTitle()
+      }
+      log('failed test "%s"', failed.fullTitle)
+      failedTests.push(failed)
+    });
+
+    // TODO: on `end` push or put file to failstore service
+  });
 }
 
 module.exports = focha
